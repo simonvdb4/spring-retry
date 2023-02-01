5b3b141eb6a756272800c1435e8cfff56d40d0eb
svdenbo1 <simon.vandenbossche@vdab.be>
2023-02-01 10:57:25 +0100

    initial commit

---
+++ src/main/java/com/vdab/retrying/RetryingApplication.java
@@ -0,0 +1,13 @@
+package com.vdab.retrying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetryingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetryingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdab/retrying/RetryingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdab.retrying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetryingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
